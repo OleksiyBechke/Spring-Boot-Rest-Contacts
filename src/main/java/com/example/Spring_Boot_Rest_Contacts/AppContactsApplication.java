7df3960953609fe7c571c3916a5617ff2d01661e
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-11-12 05:39:29 +0200

    Initial commit

---
+++ src/main/java/com/example/Spring_Boot_Rest_Contacts/AppContactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Rest_Contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContactsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Spring_Boot_Rest_Contacts/dto/contact/ContactDtoRequest.java
@@ -0,0 +1,9 @@
+package com.example.Spring_Boot_Rest_Contacts.dto.contact;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ContactDtoRequest(Long id, String firstName,
+                             String lastName, String phone) {
+}
